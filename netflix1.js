17691cff2e159e48f6a771c01f0f57a3d7444cff
bellex0 <bellepx3@gmail.com>
2020-04-20 07:11:55 -0400

    Final Netflix

--- netflix1.js
+++ netflix1.js
@@ -2,26 +2,26 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 
 async function scrape (url) {
-const browser = await puppeteer.launch({headless: true});
+const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url)
-    await page.waitForSelector('#dt-post-content')
+    // await page.waitForSelector('h2')
     
 
     var movies = await page.evaluate(() => {
-        var titleList = document.querySelectorAll('h2');
+        var titlesList = document.querySelectorAll('h2');
         // var sumList = document.querySelectorAll('h2').nextElementSibling
-        var titleListArray = [];
-        for (var i = 0; i < titleList.length; i++) {
-          titleListArray[i] = {
-            title: titleList[i].innerText.trim(),
-            summary: titleList[i].nextElementSibling.innerText.trim(),
-            moreInfo: titleList[i].nextElementSibling.nextElementSibling.nextElementSibling.innerText.replace(/\n/g, ' ').trim()
+        var movieArr = [];
+        for (var i = 0; i < titlesList.length; i++) {
+          movieArr[i] = {
+            title: titlesList[i].innerText.trim(),
+            summary: titlesList[i].nextElementSibling.innerText.trim(),
+            // moreInfo: titleList[i].nextElementSibling.nextElementSibling.nextElementSibling.innerText.replace(/\n/g, ' ').trim()
           };
         }
-        return titleListArray;
+        return movieArr;
       })
-      fs.writeFile("./netflix.json", JSON.stringify(movies), (err) => {
+      fs.writeFile("./netflix.json", JSON.stringify(movies, null, 3),  (err) => {
         if (err) {
             console.error(err);
             return;
@@ -32,9 +32,6 @@ const browser = await puppeteer.launch({headless: true});
     
     // const txt = span.getProperty('textContent')
     // const name = txt.jsonValue()
-
-    
-
 
 // console.log({newList})
 browser.close()
