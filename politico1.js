850b2f2c3455e2cd3b0d7d0887bee5b966d94234
bellex0 <bellepx3@gmail.com>
2020-04-21 19:27:59 -0400

    Added JSON file for Politico

--- politico1.js
+++ politico1.js
@@ -1,35 +1,54 @@
 const cheerio = require ('cheerio')
 const request = require ('request')
-const jsonfile = require ('jsonfile')
+// const jsonfile = require ('jsonfile')
+const fs = require('fs')
+const writeStream = fs.createWriteStream('newpolitico.json')
+
 
     var numTrump = 0
     var numBiden = 0
 
     
-    const getName = async() => {
-    //     let nameArr=[]
+    const getName = async () => {
+        let nameArr=[]
         for (i=1; i <= 10; i++){
-           await request(`https://www.politico.com/news/2020-elections/${i}`, (error, response, html) =>{
+            await request(`https://www.politico.com/news/2020-elections/${i}`, (error, response, html) =>{
                 if (!error && response.statusCode === 200){
                     const $ = cheerio.load(html)
-
-                    // $('.content.layout-bi-unequal').each((i, el) => {
-                        const name = $('h1 > a').text().replace(/\n/g, ' ')
-
+                    var arr = []
+                    $('.content.layout-bi-unequal').each((i, el) => {
+                        const name = $('.category').next().text().trim().replace(/\n/g, ", ")
+                        
+                        // console.log()
+                        // writeStream.write(JSON.stringify(data))
+                        //  var full = name+","
+                        //  nameArr.push(full)
 
                         // const name = $(el)
                         //  .find('h1')
-                        
+                        // console.log(arr)
                         
                         let data = {}
 
                         data.title = name
+                        writeStream.write(JSON.stringify(data, null, 4))
+                        // console.log(data)
+                        // console.log(arr)
                         
-                        return data
-                    }
+                        // return data
+                    //     for (var i = 0; i < nameArr.length; i++) {
+                    //         arr[i] = {
+                    //        title: name
+                    //      };
+                    //     }
+                    //     // console.log(arr, nameArr)
+                    })
+                    // console.log(data)
+                    // return arr
                     
-                   
-           })      
+                }
+                // console.log(data)
+        })   
         
             // console.log(results)
             // return results
@@ -39,6 +58,18 @@ const jsonfile = require ('jsonfile')
     // }
       
 }          
-console.log(getName())
+getName()
+
+// fs.readFile('./newpolitico.json', 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err)
+//       return
+//     }
+//     console.log(data)
+//   })
+// }
+
+// getFile()
+  
             
    
--- polpuppet.js
+++ polpuppet.js
@@ -23,35 +23,46 @@ var fs = require("fs");
 // }
 // scrapeProduct()
 
-// async function getTitles() {
-//     try {
-//       // open the headless browser
-//       var browser = await puppeteer.launch({ headless: true });
-//       // open a new page
-//       var page = await browser.newPage();
-//       // enter url in page
-//       for(i=1; i<=10; i++){
-//       await page.goto(`https://www.politico.com/news/2020-elections/${i}`);
-//       await page.waitForSelector("h1 > a");
+async function getTitles() {
+    
+      // open the headless browser
+      var browser = await puppeteer.launch({ headless: true });
+      // open a new page
+      var page = await browser.newPage();
+      // enter url in page
+      for(i=1; i<=5; i++){
+      await page.goto(`https://www.politico.com/news/2020-elections/${i}`);
+      await page.waitForSelector("h1 > a");
   
-//       var news = await page.evaluate(() => {
-//         var titleNodeList = document.querySelectorAll('h1 > a');
-//         var titleLinkArray = [];
-//         for (var i = 0; i < titleNodeList.length; i++) {
-//           titleLinkArray[i] = {
-//             title: titleNodeList[i].innerText.trim()
-//         };
-//         }
-//         console.log(titleLinkArray)
-//         return titleLinkArray;
-//       })
-//     }
-//       await browser.close();
-//       // Writing the news inside a json file
-//     //   fs.writeFile("politico.json", JSON.stringify(news), function(err) {
-//     //     if (err) throw err;
-//     //     console.log("Saved!");
-//     //   });
+      var news = await page.evaluate(() => {
+        var titleNodeList = document.querySelectorAll('h1 > a');
+        var titleLinkArray = [];
+        for (var i = 0; i < titleNodeList.length; i++) {
+          titleLinkArray[i] = {
+            title: titleNodeList[i].innerText.trim()
+        };
+        }
+        console.log(titleLinkArray)
+        return titleLinkArray;
+      })
+    }
+    fs.writeFile("./polpuppet.json", JSON.stringify(news, null, 3),  (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        console.log("Great Success");
+    });
+
+    browser.close()
+
+}
+    //   await browser.close();
+      // Writing the news inside a json file
+//       fs.writeFile("politico.json", JSON.stringify(news), function(err) {
+//         if (err) throw err;
+//         console.log("Saved!");
+//       });
 //       console.log("Browser Closed");
 //     } catch (err) {
 //       // Catch and display errors
@@ -61,33 +72,33 @@ var fs = require("fs");
 //     }
 //   };
 
-// console.log(getTitles())
+console.log(getTitles())
 
-let scrape = async () => {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
+// let scrape = async () => {
+//     const browser = await puppeteer.launch({headless: true});
+//     const page = await browser.newPage();
 
-    for(i=1; i<=10; i++){
-    await page.goto(`https://www.politico.com/news/2020-elections/${i}`)
-    await page.waitForSelector('h1')
+//     for(i=1; i<=10; i++){
+//     await page.goto(`https://www.politico.com/news/2020-elections/${i}`)
+//     await page.waitForSelector('h1')
 
-    const result = await page.evaluate(() => {
-        let titleList = document.querySelectorAll('h1');
-        var titleArray = [];
-        titleArray.push(titleList)
+//     const result = await page.evaluate(() => {
+//         let titleList = document.querySelectorAll('h1');
+//         var titleArray = [];
+//         titleArray.push(titleList)
         
-        //       })
-        // console.log(titleArray)
-        // return titleArray;
-        return titleArray;
-    });
-}
-browser.close();
-return result;
+//         //       })
+//         // console.log(titleArray)
+//         // return titleArray;
+//         return titleArray;
+//     });
+// }
+// browser.close();
+// return result;
 
 
-};
+// };
 
-scrape().then((value) => {
-    console.log(value); // Success!
-});
+// scrape().then((value) => {
+//     console.log(value); // Success!
+// });
